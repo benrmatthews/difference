b5b8ae3727f236296adb75d50bd62dc6014256eb
benrmatthews <ben@montfort.io>
2015-02-15 18:41:01 +0000

    Added jquery masonry and box design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require jquery.masonry.min.js
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
